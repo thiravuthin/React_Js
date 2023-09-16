8f36a8831c3427741746d2e5075eadc2e9e1d16f
vuthin <thiravuthin.thoeun@cubetiqs.com>
2023-09-16 23:22:15 +0700

    update

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ButtonPrimary from './utils/ButtonPrimary'
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello</h1>
+      <ButtonPrimary> </ButtonPrimary>
+    </div>
+  )
+}
